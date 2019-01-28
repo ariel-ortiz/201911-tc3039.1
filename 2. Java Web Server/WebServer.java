5c17140423ab257d493aa3de4835ce261d05ca59
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-01-28 16:32:19 -0600

    Clases que implementan un servidor web no concurrente.

---
+++ 2. Java Web Server/WebServer.java
@@ -0,0 +1,43 @@
+/**
+ * @author Ariel Ortiz
+ * 
+ * A very simple web server.
+ * Implements a minuscule subset of RFC 1945 (HTTP/1.0)
+ * http://www.ietf.org/rfc/rfc1945.txt 
+ */
+
+package mx.tec.tc3039.http;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Starts the web server and listens to client connections. Individual
+ * connections are processed by Worker instances.
+ */
+public class WebServer {
+
+    public static final int PORT_NUMBER = 12345;
+
+    private void listen() {
+        System.out.printf(
+                "Minuscule web server ready. Listening at port %d...%n",
+                PORT_NUMBER);
+
+        try (ServerSocket servSock = new ServerSocket(PORT_NUMBER)) {
+
+            while (true) {
+                Socket sock = servSock.accept();
+                Worker w = new Worker(sock);
+                w.doWork();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new WebServer().listen();
+    }
+}
---
+++ 2. Java Web Server/Worker.java
@@ -0,0 +1,75 @@
+/**
+ * @author Ariel Ortiz
+ */
+
+package mx.tec.tc3039.http;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+
+/**
+ * Processes a client connection. Reads the request information from a
+ * socket and produces a simple response.
+ */
+public class Worker {
+
+    public static final String EOL = "\r\n";
+    private Socket sock;
+
+    public Worker(Socket sock) {
+        this.sock = sock;
+    }
+
+    public void doWork() {
+
+        String content = "Hello World!" + EOL;
+
+        try (InputStream input = sock.getInputStream();
+                PrintStream output = new PrintStream(
+                        sock.getOutputStream())) {
+
+            System.out
+                    .println("-------------------------------------");
+            System.out.println(readInputBytes(input));
+
+            output.print("HTTP/1.0 200 OK" + EOL);
+            output.print("Content-type: text/plain" + EOL);
+            output.print("Content-length: " + content.length() + EOL);
+            output.print(EOL);
+            output.print(content);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            try {
+                sock.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // A utility function (just an ugly hack)
+    private static String readInputBytes(InputStream input) {
+
+        try {
+            while (true) {
+                int n = input.available();
+                if (n == 0) {
+                    Thread.sleep(50);
+                    continue;
+                }
+                byte[] bytes = new byte[n];
+                input.read(bytes);
+                return new String(bytes);
+            }
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
